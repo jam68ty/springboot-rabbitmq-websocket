67ffaf5309f511008ed47f4f2797894d1ea54c49
jam68ty <jam68ty@gmail.com>
2022-04-29 18:32:58 +0800

    feat:websocket

--- src/main/java/com/example/websocketrabbitmqdemo/WebsocketRabbitmqDemoApplication.java
+++ src/main/java/com/example/websocketrabbitmqdemo/WebsocketRabbitmqDemoApplication.java
@@ -2,11 +2,14 @@ package com.example.websocketrabbitmqdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
 @SpringBootApplication
 @EnableJpaRepositories("com.example.websocketrabbitmqdemo.dao.repositroy")
-
+@EnableWebSocket
 public class WebsocketRabbitmqDemoApplication {
 
 	public static void main(String[] args) {
@@ -14,5 +17,9 @@ public class WebsocketRabbitmqDemoApplication {
 				.run(WebsocketRabbitmqDemoApplication.class, args);
 	}
 
+	@Bean
+	public ServerEndpointExporter serverEndpointExporter() {
+		return new ServerEndpointExporter();
+	}
 
 }
